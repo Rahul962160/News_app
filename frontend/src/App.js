4418414d80f6e48ec283a11fdcbeac93f87f3664
Rahul962160 <yadav962160@gmail.com>
2025-03-31 18:50:24 +0530

    Your commit message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 import React, { Component } from "react";
 import NavBar from "./components/NavBar";
 import News from "./components/News";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import LoadingBar from "react-top-loading-bar";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
